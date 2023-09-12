1936f4acbfaab1bcdedd576e9fcf266cca89ae1c
Sri01729 <salahari@northislandcollege.ca>
2023-09-12 23:32:18 +0530

    The following change has been madd
    1. Updated the "username" variable's value to a new value.
    2. Updated the "greeting" variable's value to a new value.
    3. Updated the both "qty" and "price" key-values in the array to the new values.

--- example.php
+++ example.php
@@ -1,12 +1,12 @@
 <?php
-$username = 'Ivy';                                   // Variable to hold username
+$username = 'Sai';                                   // Variable to hold username
 
-$greeting = 'Hello, ' . $username . '.';             // Greeting is 'Hello' + username
+$greeting = 'Hi, ' . $username . '.';             // Greeting is 'Hello' + username
 
 $offer = [                                           // Create array to hold offer
     'item'     => 'Chocolate',                       // Item on offer
-    'qty'      => 5,                                 // Quantity to buy
-    'price'    => 5,                                 // Usual price per pack
+    'qty'      => 3,                                 // Quantity to buy
+    'price'    => 6,                                 // Usual price per pack
     'discount' => 4,                                 // Offer price per pack
 ];
 
@@ -29,7 +29,7 @@ $saving      = $usual_price - $offer_price;          // Total saving
 
     <p class="sticker">Save $<?= $saving ?></p>
 
-    <p>Buy <?= $offer['qty'] ?> packs of <?= $offer['item'] ?> 
-      for $<?= $offer_price ?><br> (usual price $<?= $usual_price ?>)</p> 
+    <p>Buy <?= $offer['qty'] ?> packs of <?= $offer['item'] ?>
+      for $<?= $offer_price ?><br> (usual price $<?= $usual_price ?>)</p>
   </body>
 </html>
